4db30c23d088f2b39b89c0265517d55471e2ff22
tanzhiqiang <tzq24955@ly.com>
2016-08-10 22:50:23 +0800

    导出反射

--- ExeclDataFliter.Win/Program.cs
+++ ExeclDataFliter.Win/Program.cs
@@ -16,7 +16,7 @@ namespace ExeclDataFliter.Win
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new databox());
         }
     }
 }
